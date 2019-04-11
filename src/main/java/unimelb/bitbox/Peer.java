9167c646b9472b6e53939f2f4016e24f8fa03881
triilion <bohe_sugar@sina.com>
2019-04-11 17:43:39 +1000

    some response for the incoming connections

--- src/main/java/unimelb/bitbox/Peer.java
+++ src/main/java/unimelb/bitbox/Peer.java
@@ -1,21 +1,23 @@
 package unimelb.bitbox;
-import java.io.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import unimelb.bitbox.util.Configuration;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
-
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import unimelb.bitbox.util.Configuration;
 
 
 
@@ -24,23 +26,119 @@ class Connection implements Runnable {
     DataInputStream in;
     DataOutputStream out;
     Socket clientSocket;
+    private static AtomicInteger numOfConnections;
+    private static JSONArray ConnectedPeers;
+
+    public int getNumOfConnections() {
+        return numOfConnections.get() ;
+    }
+
     public Connection (Socket aClientSocket) {
+
         try {
             clientSocket = aClientSocket;
             in = new DataInputStream( clientSocket.getInputStream());
             out =new DataOutputStream( clientSocket.getOutputStream());
+            numOfConnections.incrementAndGet();
         } catch(IOException e) {
             System.out.println("Connection:"+e.getMessage());
         }
-    }
+}
     public void run(){
         try {           // an echo server
             System.out.println("server reading data");
             String data = in.readUTF();  // read a line of data from the stream
             JSONObject json = (JSONObject) new JSONParser().parse(data);
             String command = (String) json.get("command");
-            System.out.println(command);
-            System.out.println(data);
+
+
+            // JSONObject json = (JSONObject) new JSONParser().parse(handshake);
+            //            o.writeUTF(json.toString());
+
+              switch(command)
+            {
+                case "HANDSHAKE_REQUEST": {
+                    if (numOfConnections.get()<11)
+                    {
+
+                        JSONObject commandJson = CommandGroup.CommandMap.get("HANDSHAKE_RESPONSE");
+                        JSONObject PeerJson = (JSONObject) commandJson.get("hostport");
+                        // unnecessary handshake
+                        if(ConnectedPeers.contains(PeerJson))
+                        {
+                            commandJson = CommandGroup.CommandMap.get("INVALID_PROTOCOL");
+                        }
+                        else {
+                            ConnectedPeers.add(PeerJson);
+                            numOfConnections.incrementAndGet();
+                        }
+                        out.writeUTF(commandJson.toString());
+                    }
+                    else if(numOfConnections.get()==11)
+                    {
+                        JSONObject commandJson = CommandGroup.CommandMap.get("CONNECTION_REFUSED");
+                        out.writeUTF(commandJson.toString());
+                        try {
+                            clientSocket.close();
+                        }catch (IOException e){ }
+
+                    }
+                    break;
+                }
+
+                case "FILE_CREATE_REQUEST":
+                {
+                    // issafepathname      filenameexist     -> check the file managerment system
+                    // respond, failed-> status.  other message-> return the as the methods returns
+
+                    //success:  createFileLoader, checkshortcut
+                    // stop, use the local copy or start requesting bytes
+
+                    break;
+                }
+
+
+                case "FILE_BYTES_RESPONSE":
+                {
+                    // writefile   check filecomplete..     untill-> complete
+                    break;
+                }
+
+                case "FILE_DELETE_REQUEST":
+                {
+                    // // issafepathname      filenameexist     -> check the file managerment system
+                    //                    // respond, failed-> status.  other message-> return the as the methods returns
+                    break;
+                }
+                case "FILE_MODIFY_REQUEST":
+                {
+                    // issafepathname      filenameexist     -> check the file managerment system
+                    // respond, failed-> status.  other message-> return the as the methods returns -> response
+                    break;
+                }
+
+
+                case "DIRECTORY_CREATE_REQUEST":
+                {
+                    // issafepathname      filenameexist     -> check the file managerment system
+                    // respond, failed-> status.  other message-> return the as the methods returns
+                    break;
+                }
+
+                case "DIRECTORY_DELETE_REQUEST":
+                {
+                    break;
+                }
+
+
+                default:
+                {
+                   JSONObject commandJson = CommandGroup.CommandMap.get("INVALID_PROTOCOL");
+                    out.writeUTF(commandJson.toString());
+                }
+            }
+            // timeout expression:  and then reduce the number and delete the peer
+
         }catch (EOFException e){
             System.out.println("EOF:"+e.getMessage());
         } catch(IOException e) {
@@ -57,25 +155,45 @@ class Connection implements Runnable {
 
 // basic functions: listening and sending, need to be included as a new thread;
 // implement thread pool to deal with each incoming connection
+
+// 1, only 10 threads, when 11th come, handshake, how to refuse;
 class Listen implements Runnable
 {
     ExecutorService executor = Executors.newFixedThreadPool(10);
     private int hostingPort;
     private int numOfConnections=0;
+
     private ArrayList<String> listeningPeers = new ArrayList<>();
     public Listen (Peer peer)
     {
         hostingPort= peer.getPortNo();
     }
+    public int getNumOfConnections() {
+        return numOfConnections;
+
+    }
+    public synchronized void AddConnection()
+    {
+        numOfConnections++;
+    }
+    public synchronized void ReduceConnection()
+    {
+        numOfConnections--;
+
+    }
     public void run ()
     {
         try {
             ServerSocket serverSocket = new ServerSocket(hostingPort);
-
-            while (true) {
+            while (numOfConnections<11) {
                 System.out.println("Server listening for a connection");
                 Socket clientSocket = serverSocket.accept();
-                this.executor.execute(new Connection(clientSocket));
+                this.numOfConnections++;
+                // using submit rather than execute, cause it can allow us operate the returning future object
+                Connection c = new Connection(clientSocket);
+                //get the numOfConnection value and update the outside numof Connection
+
+                this.executor.submit(c);
             }
         } catch (IOException e) {
             System.out.println("Listen socket:" + e.getMessage());
@@ -191,7 +309,7 @@ public class Peer
         numConnection = 0;
         portNo = Integer.parseInt(Configuration.getConfigurationValue("port"));
         peers = Configuration.getConfigurationValue("peers").split(",");
-
+        CommandGroup commandGroup = new CommandGroup();
     }
 
     public static void main(String[] args ) throws IOException, NumberFormatException, NoSuchAlgorithmException
